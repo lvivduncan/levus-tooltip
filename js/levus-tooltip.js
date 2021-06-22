cf27269ab113fe3a1aaf21d60b8b44f57571f150
duncan <lvivduncan@gmail.com>
2021-06-22 16:35:09 +0300

    auto tooltip -- js for

---
+++ js/levus-tooltip.js
@@ -0,0 +1,30 @@
+// 22-06-2021
+{
+    // затримка
+    const delay = 1000;
+
+    // елементи
+    const tultip = document.querySelectorAll('.levus-tultip');
+
+    // швидше працює
+    for(let i = 0; i<tultip.length; i++){
+
+        // створили елемент
+        const span = document.createElement('span');
+
+        // додали клас для вибірки
+        span.setAttribute('class', 'content');
+
+        // отримали контент з атрибута
+        const title = tultip[i].title;
+
+        // встановили затримку
+        span.style.animationDelay = `${delay * i}ms`;
+
+        // поклали дані з атрибута у тултіп
+        span.append(title);
+
+        // додали в тег
+        tultip[i].append(span);
+    }
+}
